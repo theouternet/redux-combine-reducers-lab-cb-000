ccbe6be49aece01abb434e8fcb2fc0ae76820566
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-07-02 02:33:23 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 
+import { combineReducers } from 'redux';
+import { createStore } from "redux";
+import books from "./reducers";
+import recommendedBooks from "./reducers";
+
+
+const rootReducer = combineReducers({books, recommendedBooks});
+
+//export default rootReducer;
+
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
